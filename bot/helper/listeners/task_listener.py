415bb714246848a5e429a56cc4f947fb9bdca1d2
L3G3N7 <134828867+L3G3N7@users.noreply.github.com>
2026-09-08 20:39:56 +0530

    Fix index URL formation in drive search results

--- bot/helper/listeners/task_listener.py
+++ bot/helper/listeners/task_listener.py
@@ -651,11 +651,16 @@ class TaskListener(TaskConfig):
                     elif Config.INDEX_URL:
                         INDEX_URL = Config.INDEX_URL
                     if INDEX_URL:
-                        share_url = f"{INDEX_URL}findpath?id={dir_id}"
-                        buttons.url_button("Index Link", share_url)
-                        if mime_type.startswith(("image", "video", "audio")):
-                            share_urls = f"{INDEX_URL}findpath?id={dir_id}&view=true"
-                            buttons.url_button("🌐 View Link", share_urls)
+                        url_path = rutils.quote(f"{name}")
+                        share_url = f"{INDEX_URL}/{url_path}"
+                        if mime_type == "Folder":
+                            share_url += "/"
+                            buttons.url_button("Index Link", share_url)
+                        else:
+                            buttons.url_button("Index Link", share_url)
+                            if mime_type.startswith(("image", "video", "audio")):
+                                share_urls = f"{INDEX_URL}/{url_path}?a=view"
+                                buttons.url_button("🌐 View Link", share_urls)
                 button = buttons.build_menu(2)
             else:
                 msg += f"\n\nPath: <code>{rclone_path}</code>"
--- bot/helper/mirror_leech_utils/gdrive_utils/search.py
+++ bot/helper/mirror_leech_utils/gdrive_utils/search.py
@@ -146,7 +146,14 @@ class GoogleDriveSearch(GoogleDriveHelper):
                     msg += f"📁 <code>{file.get('name')}<br>(folder)</code><br>"
                     msg += f"<b><a href={furl}>Drive Link</a></b>"
                     if index_url:
-                        url = f"{index_url}findpath?id={file.get('id')}"
+                        if isRecur:
+                            url_path = "/".join(
+                                rutils.quote(n, safe="")
+                                for n in self.__get_recursive_list(file, dir_id)
+                            )
+                        else:
+                            url_path = rutils.quote(f"{file.get('name')}", safe="")
+                        url = f"{index_url}/{url_path}/"
                         msg += f' <b>| <a href="{url}">Index Link</a></b>'
                 elif mime_type == "application/vnd.google-apps.shortcut":
                     furl = self.G_DRIVE_DIR_BASE_DOWNLOAD_URL.format(file.get("id"))
@@ -159,12 +166,17 @@ class GoogleDriveSearch(GoogleDriveHelper):
                     msg += f"📄 <code>{file.get('name')}<br>({get_readable_file_size(int(file.get('size', 0)))})</code><br>"
                     msg += f"<b><a href={furl}>Drive Link</a></b>"
                     if index_url:
-                        url = f"{index_url}findpath?id={file.get('id')}"
+                        if isRecur:
+                            url_path = "/".join(
+                                rutils.quote(n, safe="")
+                                for n in self.__get_recursive_list(file, dir_id)
+                            )
+                        else:
+                            url_path = rutils.quote(f"{file.get('name')}", safe="")
+                        url = f"{index_url}/{url_path}"
                         msg += f' <b>| <a href="{url}">Index Link</a></b>'
                         if mime_type.startswith(("image", "video", "audio")):
-                            urlv = (
-                                f"{index_url}findpath?id={file.get('id')}&view=true"
-                            )
+                            urlv = f"{index_url}/{url_path}?a=view"
                             msg += f' <b>| <a href="{urlv}">View Link</a></b>'
                 msg += "<br><br>"
                 contents_no += 1
